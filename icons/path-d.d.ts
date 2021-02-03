54b6071390bd3e604e5c197f4b2f4f23c54e7e03
Gregor Adams <gregor.adams@evernest.com>
2021-02-03 11:47:50 +0100

    feat: sort icons and provide types

---
+++ icons/path-d.d.ts
@@ -0,0 +1,55 @@
+interface Icons {
+    "16": {
+        "360Grad": string;
+        "academy": string;
+        "arrowDown": string;
+        "cta": string;
+        "digitaleTools": string;
+        "entfernen": string;
+        "guterVerdienst": string;
+        "info": string;
+        "list": string;
+        "location": string;
+        "mail": string;
+        "map": string;
+        "marketing": string;
+        "onboarding": string;
+        "verkaeuferkontakt": string;
+    };
+    "24": {
+        "360Grad": string;
+        "academy": string;
+        "arrowDown": string;
+        "cta": string;
+        "close": string;
+        "digitaleTools": string;
+        "entfernen": string;
+        "guterVerdienst": string;
+        "info": string;
+        "list": string;
+        "location": string;
+        "mail": string;
+        "map": string;
+        "marketing": string;
+        "onboarding": string;
+        "verkaeuferkontakt": string;
+    };
+    "40": {
+        "360Grad": string;
+        "academy": string;
+        "arrowDown": string;
+        "cta": string;
+        "digitaleTools": string;
+        "entfernen": string;
+        "guterVerdienst": string;
+        "info": string;
+        "list": string;
+        "location": string;
+        "mail": string;
+        "map": string;
+        "marketing": string;
+        "onboarding": string;
+        "verkaeuferkontakt": string;
+    };
+}
+export default Icons;
--- rename.js
+++ rename.js
@@ -4,26 +4,50 @@ const globby = require("globby");
 const { paramCase } = require("param-case");
 const { camelCase } = require("camel-case");
 const transliterate = require("@sindresorhus/transliterate");
-
+const mkdirp = require("mkdirp");
 const { readFile, writeFile } = pify(require("fs"));
 
 (async () => {
 	const paths = await globby(["./icons/optimized"]);
-	const pathD = {};
+	const types = { 16: {}, 24: {}, 40: {} };
+	const pathD = { 16: {}, 24: {}, 40: {} };
+	const files = { 16: {}, 24: {}, 40: {} };
 	await Promise.all(
 		paths.map(async file => {
-			const { name, base } = path.parse(file);
-			const paramCased = paramCase(transliterate(base));
-			const camelCased = camelCase(transliterate(name));
+			const { name } = path.parse(file);
+			const [, iconName, size] = /(.*) (\d+)px/.exec(name);
+			const camelCased = camelCase(transliterate(iconName));
 			const content = await readFile(file, "utf-8");
 			const [, d] = /d="([\w\d\-\s\.]+)"/.exec(content);
-			pathD[camelCased] = d;
-			console.log(`Writing: ./icons/out/${paramCased}`);
-			return writeFile(`./icons/out/${paramCased}`, content);
+			types[size][camelCased] = "string";
+			pathD[size][camelCased] = d;
+			files[size][camelCased] = content;
+			// console.log(`Writing: ./icons/out/${paramCased}.svg`);
+			// return writeFile(`./icons/out/${paramCased}.svg`, content);
 		})
 	);
+	Object.entries(files).forEach(entry => {
+		const [size, icons] = entry;
+		Object.entries(icons).forEach(async icon => {
+			const [name, content] = icon;
+			await mkdirp(`./icons/out/${size}`).catch(err => {
+				console.error("mkdirp", err);
+			});
+			writeFile(`./icons/out/${size}/${name}.svg`, content).catch(err => {
+				console.error(err);
+			});
+		});
+	});
 	console.log(`Writing: ./icons/path-d.json`);
 	writeFile(`./icons/path-d.json`, JSON.stringify(pathD, null, 4));
 	console.log(`Writing: ./icons/path-d.js`);
 	writeFile(`./icons/path-d.js`, `module.exports = ${JSON.stringify(pathD, null, 4)}`);
+	console.log(`Writing: ./icons/path-d.d.ts`);
+	writeFile(
+		`./icons/path-d.d.ts`,
+		`interface Icons ${JSON.stringify(types, null, 4)
+			.replace(/"string"(,)?/g, "string;")
+			.replace(/}(,)?/g, "};")
+			.replace(/^};$/gm, "}")}\nexport default Icons;`
+	);
 })();
