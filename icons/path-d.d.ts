c286aa2695430afab91f0512fd6506396e8751db
Adrian Payne <definitive@outlook.com>
2021-02-08 15:16:27 +0100

    feat: add icons

--- icons/path-d.d.ts
+++ icons/path-d.d.ts
@@ -4,7 +4,7 @@ export enum IconSize {
 	m = "24",
 	l = "40",
 }
-export type IconName = "360" | "academy" | "arrowRight" | "chevronDown" | "delete" | "info" | "list" | "location" | "logout" | "mail" | "map" | "mobileDevice" | "money" | "onboarding" | "person" | "star"
+export type IconName = "360" | "academy" | "arrowRight" | "chevronDown" | "delete" | "info" | "list" | "location" | "logout" | "mail" | "map" | "mobileDevice" | "money" | "onboarding" | "person" | "star" | "facebook" | "instagram" | "linkedIn" | "xing"
 export type IconCollection = {
 	[key in IconName]: string;
 }
