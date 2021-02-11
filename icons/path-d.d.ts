98a3602624d636630e23357156f77577143bdc6c
Gregor Adams <gregor.adams@evernest.com>
2021-02-11 13:51:17 +0100

    chore: run lint-staged

--- icons/path-d.d.ts
+++ icons/path-d.d.ts
@@ -1,15 +1,41 @@
-
 export enum IconSize {
 	s = "16",
 	m = "24",
 	l = "40",
 }
-export type IconName = "360" | "academy" | "bell" | "arrowRight" | "chevronDown" | "chevronLeft" | "chevronRight" | "chevronUp" | "delete" | "info" | "instagram" | "list" | "location" | "facebook" | "logout" | "linkedIn" | "mail" | "map" | "money" | "mobileDevice" | "onboarding" | "person" | "phonebook" | "plusBox" | "star" | "world" | "xing"
+export type IconName =
+	| "360"
+	| "academy"
+	| "bell"
+	| "arrowRight"
+	| "chevronDown"
+	| "chevronLeft"
+	| "chevronRight"
+	| "chevronUp"
+	| "delete"
+	| "info"
+	| "instagram"
+	| "list"
+	| "location"
+	| "facebook"
+	| "logout"
+	| "linkedIn"
+	| "mail"
+	| "map"
+	| "money"
+	| "mobileDevice"
+	| "onboarding"
+	| "person"
+	| "phonebook"
+	| "plusBox"
+	| "star"
+	| "world"
+	| "xing";
 export type IconCollection = {
 	[key in IconName]: string;
-}
+};
 export type Icons = {
 	[key in IconSize]: IconCollection;
-}
-export declare const icons: Icons
-export default icons
+};
+export declare const icons: Icons;
+export default icons;
