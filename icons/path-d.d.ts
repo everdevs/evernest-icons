518c100c026656f6650e1d993b323ef655b83704
Gregor Adams <gregor.adams@evernest.com>
2021-03-23 17:13:25 +0100

    feat: add phone icon

--- icons/path-d.d.ts
+++ icons/path-d.d.ts
@@ -4,7 +4,7 @@ export enum IconSize {
 	m = "24",
 	l = "40",
 }
-export type IconName = "360" | "academy" | "arrowRight" | "chevronDown" | "bell" | "chevronLeft" | "chevronRight" | "chevronUp" | "click" | "delete" | "filter" | "facebook" | "handshake" | "info" | "instagram" | "landArea" | "linkedIn" | "list" | "livingSpace" | "location" | "logout" | "mail" | "map" | "mapLayers" | "money" | "mobileDevice" | "onboarding" | "person" | "phonebook" | "rooms" | "plusBox" | "star" | "video" | "world" | "xing"
+export type IconName = "360" | "academy" | "arrowRight" | "bell" | "chevronDown" | "chevronLeft" | "chevronRight" | "chevronUp" | "click" | "delete" | "facebook" | "filter" | "handshake" | "instagram" | "info" | "landArea" | "list" | "livingSpace" | "linkedIn" | "location" | "logout" | "mail" | "map" | "mapLayers" | "mobileDevice" | "money" | "onboarding" | "person" | "phonebook" | "phone" | "plusBox" | "rooms" | "star" | "video" | "world" | "xing"
 export type IconCollection = {
 	[key in IconName]: string;
 }
