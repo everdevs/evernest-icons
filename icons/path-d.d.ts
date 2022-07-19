f12c70dcbb52c44154c312f71aeecada5e188230
GiulianoMontisEvernest <giuliano.montis@evernest.com>
2022-07-19 14:07:11 +0200

    fix: correct `buildingYear` icons

--- icons/path-d.d.ts
+++ icons/path-d.d.ts
@@ -4,7 +4,7 @@ export enum IconSize {
 	m = "24",
 	l = "40",
 }
-export type IconName = "1" | "2" | "3" | "360" | "4" | "5" | "6" | "7" | "8" | "9" | "academy" | "arrowRight" | "bell" | "building" | "buildingYear" | "chevronDown" | "chevronLeft" | "chevronRight" | "chevronUp" | "click" | "delete" | "facebook" | "filter" | "handshake" | "home" | "info" | "instagram" | "landArea" | "linkedIn" | "list" | "livingSpace" | "location" | "logout" | "mail" | "map" | "mapLayers" | "mobileDevice" | "money" | "onboarding" | "person" | "phone" | "phonebook" | "plot" | "plus" | "plusBox" | "rooms" | "star" | "tip" | "twitter" | "video" | "world" | "xing"
+export type IconName = "2" | "1" | "360" | "3" | "4" | "7" | "6" | "5" | "8" | "9" | "academy" | "arrowRight" | "bell" | "buildingYear" | "building" | "chevronDown" | "chevronLeft" | "chevronRight" | "chevronUp" | "click" | "delete" | "filter" | "facebook" | "handshake" | "home" | "info" | "instagram" | "landArea" | "linkedIn" | "list" | "livingSpace" | "location" | "logout" | "mail" | "map" | "mapLayers" | "mobileDevice" | "money" | "onboarding" | "person" | "phone" | "phonebook" | "plot" | "plus" | "plusBox" | "rooms" | "star" | "tip" | "twitter" | "video" | "world" | "xing"
 export type IconCollection = {
 	[key in IconName]: string;
 }
