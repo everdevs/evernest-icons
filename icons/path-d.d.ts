04499e8eb325542525800890eb3b9af5e5a4dec2
Adrian Payne <adrian.payne@evernest.com>
2023-03-28 09:35:25 +0200

    feat: add 24px euro icon

--- icons/path-d.d.ts
+++ icons/path-d.d.ts
@@ -4,7 +4,7 @@ export enum IconSize {
 	m = "24",
 	l = "40",
 }
-export type IconName = "1" | "2" | "3" | "360" | "4" | "5" | "6" | "7" | "8" | "9" | "academy" | "arrowRight" | "bell" | "building" | "buildingYear" | "chevronDown" | "chevronLeft" | "chevronRight" | "chevronUp" | "click" | "delete" | "filter" | "handshake" | "facebook" | "home" | "info" | "landArea" | "list" | "instagram" | "livingSpace" | "location" | "linkedIn" | "logout" | "mail" | "map" | "mapLayers" | "mobileDevice" | "money" | "onboarding" | "person" | "phone" | "phonebook" | "plot" | "plusBox" | "rooms" | "star" | "starFilled" | "plus" | "tip" | "twitter" | "video" | "world" | "xing"
+export type IconName = "2" | "3" | "360" | "4" | "1" | "5" | "6" | "7" | "8" | "9" | "academy" | "arrowRight" | "bell" | "building" | "buildingYear" | "chevronDown" | "chevronLeft" | "chevronRight" | "chevronUp" | "click" | "delete" | "filter" | "handshake" | "euro" | "home" | "facebook" | "info" | "instagram" | "landArea" | "list" | "linkedIn" | "livingSpace" | "location" | "logout" | "mail" | "map" | "mapLayers" | "mobileDevice" | "money" | "onboarding" | "person" | "phone" | "phonebook" | "plot" | "plusBox" | "plus" | "star" | "rooms" | "starFilled" | "tip" | "twitter" | "video" | "world" | "xing"
 export type IconCollection = {
 	[key in IconName]: string;
 }
